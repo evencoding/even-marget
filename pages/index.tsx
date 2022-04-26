96870d7231ba83141c4daa95ec5b2f2afa96e843
evencoding <evencoding@gmail.com>
2022-04-26 23:20:24 +0900

    Tailwind Practice.3

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="grid gap-10 space-y-5 bg-slate-400 py-20 px-20">
+    <div className="grid min-h-screen gap-10 space-y-5 bg-slate-400 py-20 px-20">
       <div className="rounded-3xl bg-white p-6 shadow-2xl">
         <div className="mb-1 text-2xl font-semibold">Select Item</div>
         <div className="flex justify-between">
@@ -43,8 +43,43 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="rounded-2xl bg-white p-10 shadow-2xl"></div>
-      <div className="rounded-2xl bg-white p-10 shadow-2xl"></div>
+      <div className="rounded-2xl bg-white p-6 shadow-2xl">
+        <div className="mb-5 flex items-center justify-between">
+          <span>←</span>
+          <div className="space-x-3">
+            <span>⭐️ 4.9</span>
+            <span className="rounded-md p-2 shadow-xl">❤️</span>
+          </div>
+        </div>
+        <div className="mb-5 h-72 bg-zinc-400" />
+        <div className="flex flex-col">
+          <span className="-mb-1 text-xl font-medium">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+          <div className="mt-3 mb-5 flex items-center justify-between">
+            <div className="">
+              <input type="radio" />
+              <input type="radio" />
+              <input type="radio" />
+            </div>
+            <div className="flex items-center space-x-4">
+              <button className="flex aspect-square w-8 items-center justify-center rounded-lg bg-blue-200 text-xl text-gray-500">
+                -
+              </button>
+              <span>1</span>
+              <button className="flex aspect-square w-8 items-center justify-center rounded-lg bg-blue-200 text-xl text-gray-500">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <span className="text-2xl font-medium">$450</span>
+            <button className="rounded-lg bg-blue-500 py-2 px-8 text-center text-xs text-white">
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="rounded-2xl bg-white p-6 shadow-2xl"></div>
     </div>
   );
 };
